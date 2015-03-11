183a4d885398c0c5fbb31d7c1c5a516e3c88406c
berlinHsin <berlin.hsin@gmail.com>
2015-03-11 09:33:23 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+
+This is a simple implementation for Apriori ,
+
+a class algorithm in data mining .  
---
+++ apriori.py
@@ -0,0 +1,7 @@
+#encoding:utf-8
+
+def generateCandi():
+	return candi
+
+def generateRules():
+	return rules
